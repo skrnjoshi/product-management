990e716fb460ca050330bd0c7ada852bf0683ee2
Donkana Saikiran <donkanasaikiran@Donkanas-MacBook-Air.local>
2025-12-24 14:23:14 +0530

    Setup Tailwind CSS with Vite and React

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-3xl font-bold text-white">
+        Hello World
+      </h1>
+    </div>
+  )
+}
+
+export default App
